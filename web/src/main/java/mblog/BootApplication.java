a147e8577b6d24d1466b7ed93de07c68488bf577
1169060902 <1169060902@qq.com>
2018-08-19 14:19:51 +0800

    session过期时间设置

--- web/src/main/java/mblog/BootApplication.java
+++ web/src/main/java/mblog/BootApplication.java
@@ -5,7 +5,10 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.web.ErrorMvcAutoConfiguration;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * SprintBootApplication
@@ -20,6 +23,16 @@ public class BootApplication extends SpringBootServletInitializer {
         return application.sources(BootApplication.class);
     }
 
+    @Bean
+    public EmbeddedServletContainerCustomizer containerCustomizer() {
+        return new EmbeddedServletContainerCustomizer() {
+            @Override
+            public void customize(ConfigurableEmbeddedServletContainer container) {
+                // 单位为S  过期时间设置 两小时
+                container.setSessionTimeout(7200);
+            }
+        };
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BootApplication.class, args);
